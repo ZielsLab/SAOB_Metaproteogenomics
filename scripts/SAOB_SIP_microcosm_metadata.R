38241029265afc3b039aad2c59a2055b968cda8f
Elizabeth McDaniel <emcdaniel@wisc.edu>
2022-02-10 14:33:11 -0800

    reorg

--- scripts/SAOB_SIP_microcosm_metadata.R
+++ scripts/SAOB_SIP_microcosm_metadata.R
@@ -1,13 +1,15 @@
 library(tidyverse)
 library(ggpubr)
 library(viridis)
-
+library(readxl)
+library(ggpubr)
+library(cowplot)
 
 ## read in metadata from  spreadsheet, binding rows of each worksheet together
   # experimental values file path
-  meta_path = "~/Documents/UBC_Research/PostDoc/Daniel Mulat/Results/SIP_Acetate_BONCAT_Sept_2020_data_compilation.xlsx"
+  meta_path = "raw_data/SIP_Acetate_BONCAT_Sept_2020_data_compilation.xlsx"
   # microcosm metadata file path
-  bottle_meta = "~/Documents/UBC_Research/PostDoc/Daniel Mulat/Results/BONCAT-SIP Batch/SIP_Acetate_BONCAT_Sept_2020_metadata.xlsx"
+  bottle_meta = "raw_data/SIP_Acetate_BONCAT_Sept_2020_metadata.xlsx"
   
   #read files
   meta <- read_xlsx(path = meta_path, sheet = '24h', col_names = TRUE) %>%
@@ -27,7 +29,7 @@ library(viridis)
     drop_na(cum.ch4)
 
 # cumulative methane production
-ggplot(ch4, 
+ch4_plot <- ggplot(ch4, 
        aes(x=time, y=cum.ch4, color = treatment, fill = treatment)) +
   stat_summary(fun.data = 'mean_cl_boot', geom = 'ribbon', alpha = 0.1, colour = NA) +
   stat_summary(fun = "mean", geom = "point", size = 2) +  
@@ -35,11 +37,12 @@ ggplot(ch4,
   scale_color_viridis(discrete = TRUE, option = "D")+
   scale_fill_viridis(discrete = TRUE,  option = "D") +
   ylab("Cumulative methane production (mL)") + 
-  xlab("Time (hrs)") +
-  theme_pubr() 
+  xlab("Time (hrs)") + 
+  theme_bw() + theme(legend.position = "none")
+
 
 # vfa concentration
-ggplot(meta %>% 
+vfa_plot <- ggplot(meta %>% 
          filter(HPG == "no") %>%  
          drop_na(acetate_ppm, treatment), 
        aes(x=time, y=acetate_ppm, color = treatment, fill = treatment)) +
@@ -50,9 +53,33 @@ ggplot(meta %>%
   scale_fill_viridis(discrete = TRUE,  option = "D") +
   ylab("Acetate concentration (mg/L)") + 
   xlab("Time (hrs)") +
-  theme_pubr() 
+  theme_bw() 
+
+ggsave("figures/cumulative_ch4_plot_sip_timeseries.png", ch4_plot, width=12, height=8, units=c("cm"))
+
+ggsave("figures/vfa_degradation_sip_timeseries.png", vfa_plot, width=12, height=8, units=c("cm"))
+
+chem_grid <- plot_grid(ch4_plot, NULL, vfa_plot, nrow=1, labels = c("A", "", "B"), rel_widths = c(1.5, 0.05, 2))
 
 
+# now add the title
+title <- ggdraw() + 
+  draw_label(
+    "Cumulative Methane Production and VFA Uptake over the Time-Series",
+    fontface = 'bold',
+    x = 0,
+    hjust = 0
+  ) +
+  theme(
+    # add margin on the left of the drawing canvas,
+    # so title is aligned with left edge of first plot
+    plot.margin = margin(0, 0, 0, 7)
+  )
+saob_grid <- plot_grid(
+  title, chem_grid,
+  ncol = 1,
+  # rel_heights values control vertical title margins
+  rel_heights = c(0.1, 1)
+)
 
-
-
+ggsave("figures/saob_sip_experiment_metadata_grid.png", saob_grid, width=15, height=12, units=c("cm"))
