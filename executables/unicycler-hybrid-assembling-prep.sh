91e38554ffe113c5351b080559fe79af314e1adc
Elizabeth McDaniel <emcdaniel@wisc.edu>
2022-06-04 17:16:04 -0700

    finished binning method comps, script for prepping mapping files for unicycler hybrid assemblies of select bins

---
+++ executables/unicycler-hybrid-assembling-prep.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#SBATCH --account=rrg-ziels
+#SBATCH --array=1-4
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=22:0:0
+#SBATCH --job-name=unicycler-hybrid-assembly
+#SBATCH --output=%x.out
+#SBATCH --mail-user=eamcdani@mail.ubc.ca
+#SBATCH --mail-type=ALL
+
+# mapping to Nanopore and Illumina genome assemblies for select genome bins, feeding the sets of reads to Unicycler for a hybrid binning approach of the two
+# array job based on directory names
+
+# map the NP reads with minimap2 
+# map the Illumina reads from Sept 2020 sample with bowtie2
+# do this reciprocally to the assemblies 
+# dereplicate the sets of reads and feed to Unicycler 
+
+#paths
+project_path="/project/6049207/AD_metagenome-Elizabeth"
+dirs_path="${project_path}/re_binning/np_binning_v2_poly/unicycler_hybrid"
+r1_read_path="${project_path}/00_illumina_for_polishing/illumina_qced/R2Sept2020_R1.qced.fastq"
+r2_read_path="${project_path}/00_illumina_for_polishing/illumina_qced/R2Sept2020_R2.qced.fastq"
+np_read_path="${project_path}/03_porechop_trimmed/AD_SIP_trimmed_combined.fastq"
+
+# assembly directory name based from the array job
+assembly_name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" genome_directories.txt | awk -F "\t" '{print $1}')
+
+# modules
+# bowtie and minimap2 mapping, samtools, unicycler Singularity image
+module load StdEnv/2020 gcc/9.3.0 bowtie2 samtools minimap2
+
+# cd into the directory 
+${dirs_path}/${assembly_name}
+mkdir bt2
+
+# make bowtie2 indexes of each assembly for short read 
+	# np assembly
+bowtie2-build bin*.fa bt2/np_assembly
+bowtie2-build R2Sept2020*.fa bt2/ilm_assembly
+
+# map short reads to each assembly with bowtie2
+bowtie2 -x bt2/np_assembly -1 ${r1_read_path} -2 ${r2_read_path} -q --very-sensitive -p 10 -S np_assembly_vs_ilm_reads.sam 
+
+bowtie2 -x bt2/ilm_assembly -1 ${r1_read_path} -2 ${r2_read_path} -q --very-sensitive -p 10 -S ilm_assembly_vs_ilm_reads.sam
+
+# map long reads to each assembly with minimap2 
+minimap2 -t 10 -x map-ont -a bin*.fa ${np_read_path} > np_assembly_vs_long_reads.sam 
+
+minimap2 -t 10 -x map-ont -a R2Sept2020*.fa ${np_read_path} > ilm_assembly_vs_long_reads.sam 
+
+# sort and index all the SAM > BAM files 
+for file in *.sam; do
+	name=$(basename $file .sam);
+	samtools view -@ 10 -S -b $file > ${name}.bam;
+	samtools sort -@ 10 ${name}.bam -o ${name}.sorted.bam;
+	samtools index ${name}.sorted.bam;
+done 
+
+# convert every sorted BAM file to a FASTQ file of reads 
+for file in *_vs_ilm_reads.sorted.bam; do
+	name=$(basename $file .sorted.bam);
+	samtools fastq $file -1 ${name}_1.fastq -2 ${name}_2.fastq;
+done
+
+for file in *_vs_long_reads.sorted.bam; do
+	name=$(basename $file .sorted.bam);
+	samtools fastq $file -o ${name}.fastq; 
+done
+
+# mapping files generated, feed into dedupe.sh for deduplication of the short read files and long read files
+
--- scripts/binning_method_comparisons.R
+++ scripts/binning_method_comparisons.R
@@ -5,12 +5,12 @@ library(tidyverse)
 #################################################################
 
 # checkM stats for both sets of bins
-all_TS_bins_info <- read.table("results/polished_binning/binning_comparisons/automatic_binning_checkM_stats.tsv", sep="\t", col.names=c("bin1", "lineage1", "completeness1", "contamination1", "strainHeterogeneity1", "size1", "contigs1", "GC1"))
+all_TS_bins_info <- read.table("results/re_polished_binning/binning_comparisons/automatic_binning_checkM_stats.tsv", sep="\t", col.names=c("bin1", "lineage1", "completeness1", "contamination1", "strainHeterogeneity1", "size1", "contigs1", "GC1"))
 
-np_bins_info <- read.table("results/polished_binning/binning_comparisons/np_binning_checkM_stats.tsv", sep="\t", col.names=c("bin2", "lineage2", "completeness2", "contamination2", "strainHeterogeneity2", "size2", "contigs2", "GC2"))
+np_bins_info <- read.table("results/re_polished_binning/binning_comparisons/np_bins_checkm_stats.tsv", sep="\t", col.names=c("bin2", "lineage2", "completeness2", "contamination2", "strainHeterogeneity2", "size2", "contigs2", "GC2"))
 
 # ANI comparisons
-ani_comparisons <- read.table("results/polished_binning/binning_comparisons/comparisons.txt", sep="\t", col.names = c("bin1", "bin2", "ANI", "l1", "l2")) %>% 
+ani_comparisons <- read.table("results/re_polished_binning/binning_comparisons/comps.txt", sep="\t", col.names = c("bin1", "bin2", "ANI", "l1", "l2")) %>% 
   select(bin1, bin2, ANI) %>% 
   mutate(bin1 = gsub(".fa", "", bin1)) %>% 
   mutate(bin2 = gsub (".fa", "", bin2))
@@ -23,4 +23,4 @@ all_binning_comparisons <- left_join(ani_comparisons, all_TS_bins_info) %>%
   mutate(lineage = lineage1) %>% 
   select(-lineage1)
 
-write.csv(all_binning_comparisons, "results/polished_binning/binning_comparisons/all_binning_comparisons.csv", quote=FALSE, row.names = FALSE)
+write.csv(all_binning_comparisons, "results/re_polished_binning/binning_comparisons/all_final_binning_comparisons.csv", quote=FALSE, row.names = FALSE)
