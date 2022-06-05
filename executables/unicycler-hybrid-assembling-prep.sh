4b29e50bd515796977be2405ba810ed65f7bb96e
Elizabeth McDaniel <emcdaniel@wisc.edu>
2022-06-05 12:47:52 -0700

    changed to not having to dedup reads, unicycler feeding in

--- executables/unicycler-hybrid-assembling-prep.sh
+++ executables/unicycler-hybrid-assembling-prep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=rrg-ziels
 #SBATCH --array=1-4
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=40G
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=60G
 #SBATCH --time=22:0:0
 #SBATCH --job-name=unicycler-hybrid-assembly
 #SBATCH --output=%x.out
@@ -14,8 +14,7 @@
 
 # map the NP reads with minimap2 
 # map the Illumina reads from Sept 2020 sample with bowtie2
-# do this reciprocally to the assemblies 
-# dereplicate the sets of reads and feed to Unicycler 
+# bamtofastq to feed reads to Unicycler
 
 #paths
 project_path="/project/6049207/AD_metagenome-Elizabeth"
@@ -35,26 +34,20 @@ ilm_assembly=$(sed -n "${SLURM_ARRAY_TASK_ID}p" unicycler_dir_files.txt | awk -F
 
 # modules
 # bowtie and minimap2 mapping, samtools, unicycler Singularity image
-module load StdEnv/2020 gcc/9.3.0 bowtie2 samtools minimap2 bedtools
+module load StdEnv/2020 gcc/9.3.0 bowtie2 samtools minimap2 bedtools singularity
 
 # cd into the directory 
 cd ${dirs_path}/${dir_name}
 mkdir bt2
 
-# make bowtie2 indexes of each assembly for short read 
-	# np assembly
-bowtie2-build ${np_assembly} bt2/np_assembly
+# make bowtie2 index of the short read assembly
 bowtie2-build ${ilm_assembly} bt2/ilm_assembly
 
-# map short reads to each assembly with bowtie2
-bowtie2 -x bt2/np_assembly -1 ${r1_read_path} -2 ${r2_read_path} -q --very-sensitive -p 10 -S np_assembly_vs_ilm_reads.sam 
-
+# map short reads to the short read assembly
 bowtie2 -x bt2/ilm_assembly -1 ${r1_read_path} -2 ${r2_read_path} -q --very-sensitive -p 10 -S ilm_assembly_vs_ilm_reads.sam
 
-# map long reads to each assembly with minimap2 
-minimap2 -t 10 -x map-ont -a bin*.fa ${np_read_path} > np_assembly_vs_long_reads.sam 
-
-minimap2 -t 10 -x map-ont -a R2Sept2020*.fa ${np_read_path} > ilm_assembly_vs_long_reads.sam 
+# map long reads to the long read assembly
+minimap2 -t 10 -x map-ont -a ${np_assembly} ${np_read_path} > np_assembly_vs_long_reads.sam 
 
 # sort and index all the SAM > BAM files 
 for file in *.sam; do
@@ -75,5 +68,5 @@ for file in *_vs_long_reads.sorted.bam; do
 	bedtools bamtofastq -i $file -fq ${name}.fastq; 
 done # has to be done with bamtofastq because samtools fastq command expecting paired end reads and did weird things
 
-# mapping files generated, feed into dedupe.sh for deduplication of the short read files and long read files
-
+# unicycler command 
+singularity exec -B /home -B /project -B /scratch -B /localscratch ${project_path}/singularity_images/unicycler_v0.4.9.sif unicycler -1 ilm_assembly_vs_ilm_reads_1.fastq -2 ilm_assembly_vs_ilm_reads_2.fastq -l np_assembly_vs_long_reads.fastq -o ${dir_name}_unicycler
